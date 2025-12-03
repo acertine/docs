c89a7073713a4f0649d7ce87478b5616a1f2f69e
Luke Orriss <73408132+v0sudo@users.noreply.github.com>
2025-12-03 00:08:04 +0000

    Add new hostname configuration for AWS in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "img.freepik.com/**",
       },
+      {
+        protocol: "https",
+        hostname: "*.amazonaws.com",
+      },
     ],
   },
   // if used turbopack
